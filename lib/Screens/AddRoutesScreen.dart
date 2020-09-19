a6e881f456a8c65e41bb1a6e0823d27391d526af
radroof22 <mehta.rohan@outlook.com>
2020-09-19 03:16:28 -0400

    Add routes screen page

---
+++ lib/Screens/AddRoutesScreen.dart
@@ -0,0 +1,79 @@
+import 'package:Vacuate/Custom/BottomNavBar.dart';
+import 'package:flutter/material.dart';
+
+import '../Custom/page_title.dart';
+import '../constants.dart';
+import '../constants.dart';
+import '../constants.dart';
+import '../constants.dart';
+import '../constants.dart';
+
+class AddRoutesScreen extends StatefulWidget {
+  AddRoutesScreen({Key key}) : super(key: key);
+
+  _AddRoutesScreenState createState() => _AddRoutesScreenState();
+}
+
+class _AddRoutesScreenState extends State<AddRoutesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          child: ListView(
+            children: <Widget>[
+              PageTitle(
+                title: "Vacuate",
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.topLeft,
+                child: Text("Add Routes", style: subTextStyle),
+              ),
+
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+
+              Text("Add diagram map entrance here"),
+              
+
+              // Add Route Button
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              Container(padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.28, 0, MediaQuery.of(context).size.width * 0.28, 0),
+               child: ButtonTheme(
+                  height: 50,
+                  
+                  child: FlatButton(
+                  
+                  color: purpleColor,
+                  onPressed: () => print("Add Building Routes"),
+                  child: Text("Press To Start", style: standardWhiteTitle),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0), 
+                    
+                ),
+                ),
+              ), 
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Container(
+        height: 75,
+        width: 75,
+        child: RawMaterialButton(
+          shape: CircleBorder(),
+          fillColor: pinkishColor,
+          child: Icon(Icons.directions_run, color: Colors.black, size: 50),
+          onPressed: () {},
+        ),
+      ),
+      bottomNavigationBar: BottomNavBar(),
+    );
+  }
+}
