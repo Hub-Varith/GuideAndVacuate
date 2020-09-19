088db6dab75d49532d4b4dada4a0e43afba812bd
radroof22 <mehta.rohan@outlook.com>
2020-09-19 10:30:17 -0400

    Refactored Floating Action Button into own widget

---
+++ lib/Custom/fob.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+class FOB extends StatelessWidget {
+  const FOB({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        height: 75,
+        width: 75,
+        child: RawMaterialButton(
+          shape: CircleBorder(),
+          fillColor: pinkishColor,
+          child: Icon(Icons.directions_run, color: Colors.black, size: 50),
+          onPressed: () {},
+        ),
+      ),
+      
+    );
+  }
+}
