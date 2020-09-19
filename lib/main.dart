049cde10f0a59452cc060f1d1a6695c2fd7abf2a
unknown <haemavitch@gmail.com>
2020-09-19 21:28:38 +0700

    added two more routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:Vacuate/Screens/AddRoutesScreen.dart';
+import 'package:Vacuate/Screens/LoginScreen.dart';
 import 'package:Vacuate/Screens/OfficeInformationScreen.dart';
 import 'package:Vacuate/Screens/SignupScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/services.dart';
 import 'Screens/HomeScreen.dart';
+import 'package:provider/provider.dart';
+import 'Screens/HomeScreen.dart';
+import 'Screens/HomeScreen.dart';
+import 'Services/UserAuthProvider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -21,24 +29,39 @@ class MyApp extends StatelessWidget {
         DeviceOrientation.portraitDown,
       ],
     );
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: GoogleFonts.kanitTextTheme(),
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomeScreen(),
-      initialRoute: '/',
-      routes: {
-        // '/': (context) => HomeScreen(),
-        '/office_info': (context) => OfficeInformationScreen(),
-        "/add_routes": (context) => AddRoutesScreen(),
-        //Temporary 
-        '/signup_screen': (context) => SignupScreen(),
-      }
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserAuthProvider.instance(),
+        )
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            textTheme: GoogleFonts.kanitTextTheme(),
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, userSnapshot) {
+              if(userSnapshot.hasData) {
+                return HomeScreen();
+              }
+              return SignupScreen();
+            },
+          ),
+          initialRoute: '/',
+          routes: {
+            // '/': (context) => HomeScreen(),
+            '/office_info': (context) => OfficeInformationScreen(),
+            "/add_routes": (context) => AddRoutesScreen(),
+            //Temporary
+            '/signup_screen': (context) => SignupScreen(),
+            '/login_screen' : (context) => LoginScreen(),
+            '/home_screen': (context) => HomeScreen(),
+          }),
     );
   }
 }
-
