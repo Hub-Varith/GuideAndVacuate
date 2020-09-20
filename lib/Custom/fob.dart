358ee25218caf628a27fe3feda0c510cd1fc6706
unknown <haemavitch@gmail.com>
2020-09-20 13:06:10 +0700

    navigate to loading screen

--- lib/Custom/fob.dart
+++ lib/Custom/fob.dart
@@ -1,12 +1,17 @@
-
+import 'package:Vacuate/Screens/EvacuationScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:Vacuate/Services/BleService.dart';
+import 'package:Vacuate/Screens/EvacuationRouteScreen.dart';
 
 import '../constants.dart';
+
 class FOB extends StatelessWidget {
   const FOB({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var bleService = Provider.of<BtServices>(context);
     return Container(
       child: Container(
         height: 75,
@@ -16,11 +21,14 @@ class FOB extends StatelessWidget {
           fillColor: pinkishColor,
           child: Icon(Icons.directions_run, color: Colors.black, size: 50),
           onPressed: () {
-            Navigator.of(context).pushNamed("/evacuation_screen");
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => EvacuationRouteScreen(bleService: bleService,),
+              ),
+            );
           },
         ),
       ),
-      
     );
   }
 }
