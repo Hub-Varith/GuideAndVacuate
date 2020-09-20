823819230ac0bdb19a380a88c746127c38e990a1
unknown <haemavitch@gmail.com>
2020-09-20 13:06:18 +0700

    initialize ble

--- lib/Screens/EvacuationRouteScreen.dart
+++ lib/Screens/EvacuationRouteScreen.dart
@@ -1,17 +1,31 @@
+import 'package:Vacuate/Screens/EvacuationScreen.dart';
+import 'package:Vacuate/Services/BleService.dart';
 import 'package:flutter/material.dart';
 import 'package:Vacuate/constants.dart';
 import 'package:Vacuate/Custom/page_title.dart';
+import 'package:provider/provider.dart';
+import 'package:Vacuate/Services/BleService.dart';
 
 import '../constants.dart';
 
 class EvacuationRouteScreen extends StatefulWidget {
+  final BtServices bleService;
+
+  EvacuationRouteScreen({this.bleService});
+
   @override
   _EvacuationRouteState createState() => _EvacuationRouteState();
 }
 
 class _EvacuationRouteState extends State<EvacuationRouteScreen> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    widget.bleService.initializeBle().then((value) => widget.bleService.init());
+  }
+
+
+  Widget evacuationLoadingScreen() {
     return Scaffold(
       backgroundColor: bgColor,
       body: SafeArea(
@@ -28,11 +42,15 @@ class _EvacuationRouteState extends State<EvacuationRouteScreen> {
                   alignment: Alignment.topLeft,
                   child: Text("Evacuate", style: subTextStyle),
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * .1,),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .1,
+                ),
                 Container(
                     padding: EdgeInsets.all(5),
-                    child: Image.asset("assets/images/fire.png",
-                        height: MediaQuery.of(context).size.height * .25,)),
+                    child: Image.asset(
+                      "assets/images/fire.png",
+                      height: MediaQuery.of(context).size.height * .25,
+                    )),
                 SizedBox(height: MediaQuery.of(context).size.height * 0.02),
                 Text(
                   "Finding Your",
@@ -44,5 +62,14 @@ class _EvacuationRouteState extends State<EvacuationRouteScreen> {
             )),
       ),
     );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: widget.bleService.visibleDevices, builder: (context, snapshot) {
+      if(!snapshot.hasData) {
+        return evacuationLoadingScreen();
+      }
+      return EvacuationScreen();
+    });
   }
 }
