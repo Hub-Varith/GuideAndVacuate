60eeaa65b0272a11509526c6bf5c776e5d779cd7
unknown <haemavitch@gmail.com>
2020-09-20 09:35:33 +0700

    deleted unnecessary stuff

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,27 @@ import 'package:provider/provider.dart';
 import 'Services/UserAuthProvider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-var liveDb;
+FirebaseApp liveDb;
+bool USE_FIRESTORE_EMULATOR = false;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  liveDb = await Firebase.initializeApp(
-      name: 'db2',
-      options: FirebaseOptions(
-              appId: '1:455185418375:web:b244dc03c4ab7440ed1022',
-              apiKey: 'AIzaSyCOJbAKgkCEvTjHqUnEH4Oyt1NGqkiMLNQ',
-              messagingSenderId: '455185418375',
-              projectId: 'hackmit-df9ea',
-              databaseURL: 'https://hackmit-df9ea.firebaseio.com',
-            ),
-    );
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings = Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  }
+  // liveDb = await Firebase.initializeApp(
+  //   name: 'db2',
+  //   options: FirebaseOptions(
+  //     appId: '1:455185418375:web:b244dc03c4ab7440ed1022',
+  //     apiKey: 'AIzaSyCOJbAKgkCEvTjHqUnEH4Oyt1NGqkiMLNQ',
+  //     messagingSenderId: '455185418375',
+  //     projectId: 'hackmit-df9ea',
+  //     databaseURL: 'https://hackmit-df9ea.firebaseio.com',
+  //   ),
+  // );
   runApp(MyApp());
 }
 
@@ -43,10 +49,10 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (_) => UserAuthProvider.instance(),
-        )
+        ),
+        // ChangeNotifierProvider(create: (_) => )
       ],
       child: MaterialApp(
-        
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
@@ -57,7 +63,7 @@ class MyApp extends StatelessWidget {
           home: StreamBuilder(
             stream: FirebaseAuth.instance.authStateChanges(),
             builder: (ctx, userSnapshot) {
-              if(userSnapshot.hasData) {
+              if (userSnapshot.hasData) {
                 return HomeScreen();
               }
               return SignupScreen();
@@ -66,11 +72,12 @@ class MyApp extends StatelessWidget {
           initialRoute: '/',
           routes: {
             // '/': (context) => HomeScreen(),
-            '/office_info': (context) => OfficeInformationScreen(liveDb: liveDb),
+            '/office_info': (context) =>
+                OfficeInformationScreen(),
             "/add_routes": (context) => AddRoutesScreen(),
             //Temporary
             '/signup_screen': (context) => SignupScreen(),
-            '/login_screen' : (context) => LoginScreen(),
+            '/login_screen': (context) => LoginScreen(),
             '/home_screen': (context) => HomeScreen(),
             '/evacuation_screen': (context) => EvacuationScreen(),
           }),
