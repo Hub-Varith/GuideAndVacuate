17d9296296894bf65a58cd52ecafe3f97335c434
radroof22 <mehta.rohan@outlook.com>
2020-09-20 06:11:32 -0400

    tts with monoxide, lpg, and fire specific desc

--- lib/Screens/EvacuationScreen.dart
+++ lib/Screens/EvacuationScreen.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:Vacuate/Custom/highlightedText.dart';
 import 'package:Vacuate/Custom/page_title.dart';
+import 'package:Vacuate/Services/SensorProvider.dart';
 import 'package:Vacuate/Services/TTSService.dart';
+import 'package:Vacuate/models/sensorData.dart';
 import 'package:Vacuate/positional_tracking/device.dart';
 import 'package:Vacuate/positional_tracking/room.dart';
 import 'package:Vacuate/positional_tracking/sensor_handler.dart';
 import 'package:Vacuate/visual/room_visual.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:Vacuate/constants.dart';
 import 'package:provider/provider.dart';
@@ -21,6 +27,12 @@ class _EvacuationScreenState extends State<EvacuationScreen> {
   var room;
   TTSService tts;
 
+  SensorData _sensor;
+  DatabaseReference _sensorRef;
+  StreamSubscription<Event> _sensorSubscription;
+  DatabaseError _error;
+  SensorProvider sensorProvider;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -28,8 +40,43 @@ class _EvacuationScreenState extends State<EvacuationScreen> {
     this.device = Device();
     this.sensorHandler = SensorHandler(this.device);
     this.room = Room();
+
+    sensorProvider = SensorProvider();
+    
+    this._sensorRef = FirebaseDatabase.instance.reference().child("SensorData");
+    _sensorRef.keepSynced(true);
+    _sensorSubscription = _sensorRef.onValue.listen((Event e) {
+      setState((){
+        _error = null;
+        _sensor = SensorData.fromJson(e.snapshot.value) ?? "";
+        sensorProvider.postSensorData(_sensor);
+      });
+    }, onError: (Object o) {
+      final DatabaseError error = o;
+      setState(() {
+        _error = error; 
+      });
+    });
+
     tts = TTSService();
-    tts.getTTS("Please proceed to the nearest exit, highlighted on the map!");
+    tts.getTTS("Please proceed to the nearest exit, highlighted on the map!").then((_) async =>{
+      if(_sensor.Monoxide > 4){
+        await tts.getTTS("Carbon Monoxide is present in high quantities. Be aware of your breathing!"),
+      },
+      if(_sensor.LPG > 3){
+        await tts.getTTS("Gas is present large quantities. Be aware of the possibility of an explosion."),
+      },
+      if(_sensor.FIRE == true){
+        await tts.getTTS("Fire is PRESENT. I repeat, Fire is present!")
+      }
+    });
+  }
+  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _sensorSubscription.cancel();
   }
 
   @override
@@ -38,7 +85,7 @@ class _EvacuationScreenState extends State<EvacuationScreen> {
     // var roomPainter = Provider.of<RoomVisualPainter>(context);
     return Scaffold(
       backgroundColor: bgColor,
-      body: SafeArea(
+      body:  (this._sensor != null) ? SafeArea(
         child: Container(
           width: double.infinity,
           height: MediaQuery.of(context).size.height,
@@ -105,7 +152,7 @@ class _EvacuationScreenState extends State<EvacuationScreen> {
             ],
           ),
         ),
-      ),
+      ) : Text("Loading..."),
     );
   }
 }
