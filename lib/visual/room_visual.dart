8bd89680978cd57b09e8e4701f01ca2804d0b93b
unknown <haemavitch@gmail.com>
2020-09-20 13:06:54 +0700

    reformatted

--- lib/visual/room_visual.dart
+++ lib/visual/room_visual.dart
@@ -1,24 +1,28 @@
+import 'package:Vacuate/constants.dart';
 import 'package:Vacuate/positional_tracking/room.dart';
 import 'package:Vacuate/positional_tracking/sensor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:Vacuate/Services/BleService.dart';
 
 class RoomVisualPainter extends CustomPainter {
-  RoomVisualPainter({this.sensors, this.device, this.room});
+  BtServices bleService;
+  RoomVisualPainter({this.sensors, this.device, this.room, this.bleService});
 
-  
   List<Sensor> sensors;
   var device;
   Room room;
+
   @override
-  void paint(Canvas canvas, Size size){
+  void paint(Canvas canvas, Size size) {
     var paint = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = 4.0
       ..color = Colors.black;
-    
+
     // draw boundaries of rooms
-    for(int i = 0; i < sensors.length; i++){
+    for (int i = 0; i < sensors.length; i++) {
       canvas.drawRect(
         Rect.fromPoints(Offset(sensors[i].x, sensors[i].y), Offset(150, 150)),
         paint,
@@ -26,41 +30,37 @@ class RoomVisualPainter extends CustomPainter {
     }
 
     // draw each sensor
-    paint ..color = Colors.yellow;
-    for(int i = 0; i < sensors.length; i++){
+    paint..color = Colors.yellow;
+    for (int i = 0; i < sensors.length; i++) {
       canvas.drawCircle(Offset(sensors[i].x, sensors[i].y), 2, paint);
     }
 
     // Draw the Device
-    paint ..color = Colors.blue;
+    paint..color = Colors.blue;
     canvas.drawCircle(Offset(device[0], device[1]), 5, paint);
-    
+
     // Doorways
-    paint ..color = Colors.green;
-    for(var doorway in room.doorWays){
+    paint..color = Colors.green;
+    for (var doorway in room.doorWays) {
       canvas.drawRect(
-        Rect.fromLTWH(doorway[0], doorway[1], doorway[2], doorway[3]),
-        paint
-      );
+          Rect.fromLTWH(doorway[0], doorway[1], doorway[2], doorway[3]), paint);
     }
 
-    paint ..color = Colors.white;
-    for(var intDoor in room.intermediateDoors){
+    paint..color = Colors.white;
+    for (var intDoor in room.intermediateDoors) {
       canvas.drawRect(
-        Rect.fromLTWH(intDoor[0], intDoor[1], intDoor[2], intDoor[3]),
-        paint
-      );
+          Rect.fromLTWH(intDoor[0], intDoor[1], intDoor[2], intDoor[3]), paint);
     }
 
     // Pick optimal route of exit
     var spots = room.findPath(sensors, device);
-    paint ..color = Colors.pink;
+    paint..color = Colors.pink;
     var old = device;
-    for(int i = 0; i < spots.length; i++){
-      canvas.drawLine(Offset(old[0], old[1]), Offset(spots[i][0], spots[i][1]), paint);
+    for (int i = 0; i < spots.length; i++) {
+      canvas.drawLine(
+          Offset(old[0], old[1]), Offset(spots[i][0], spots[i][1]), paint);
       old = [spots[i][0], spots[i][1]];
     }
-
   }
 
   @override
@@ -68,5 +68,4 @@ class RoomVisualPainter extends CustomPainter {
     // TODO: implement shouldRepaint
     return false;
   }
-
 }
